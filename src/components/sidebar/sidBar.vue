668d0df858e00a1a8472090749f8b056b70de85c
해준 <127281057+ftery0@users.noreply.github.com>
2024-10-07 15:18:32 +0900

    사이드바 구조 변경

--- src/components/sidebar/sidBar.vue
+++ src/components/sidebar/sidBar.vue
@@ -1,29 +1,11 @@
 <template>
-  <header v-if="isMobile" class="header">
-    <button @click="toggleSidebar" class="toggle-button">☰</button>
-    <h1 class="logo">My App</h1>
-  </header>
-
-  <aside :class="['sidebar', { active: showSidebar }]">
-    <button @click="toggleSidebar" class="close-button">✖</button>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
+  <aside class="sidebar">
+    <div class="logo_box">
+      <router-link to="/">
+        <div></div>
+      </router-link>
+    </div>
   </aside>
-
-  <main class="content" :class="{ 'is-mobile': isMobile }">
-    <h2>Main Content Area</h2>
-    <p>Your main content goes here.</p>
-  </main>
-
-  <transition name="fade">
-    <div v-if="showSidebar" class="overlay" @click="toggleSidebar"></div>
-  </transition>
 </template>
 
 <script>
@@ -54,89 +36,43 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  background-color: #333;
-  color: white;
-  position: relative;
-}
-
 .sidebar {
-  width: 250px;
-  background-color: #333;
-  color: white;
+  z-index: 101;
+  transform: translateX(-100vw);
+  background-color: var(--InterestExistence-colors-brand-background);
+  border-right-color: var(--InterestExistence-colors-brand-highlight);
+  width: var(--InterestExistence-sizes-full);
   position: fixed;
-  height: 100%;
-  left: 0;
-  top: 0;
-  transition: transform 0.3s ease;
-  transform: translateX(-100%);
-  z-index: 100;
-
-  &.active {
-    transform: translateX(0);
-  }
-
-  .close-button {
-    font-size: 2rem;
-    cursor: pointer;
-    padding: 1rem;
-  }
-
-  nav {
-    ul {
-      list-style-type: none;
-      padding: 0;
-
-      li {
-        padding: 1rem;
-        a {
-          color: white;
-          text-decoration: none;
-          &:hover {
-            text-decoration: underline;
-          }
-        }
-      }
+  height: 100vh;
+  overflow-y: hidden;
+  .logo_box {
+    display: flex;
+    height: 5rem;
+    -webkit-box-align: center;
+    align-items: center;
+    margin-inline-start: var(--chakra-space-8);
+    margin-inline-end: var(--chakra-space-8);
+    margin-bottom: 12px;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    a {
+      transition-property: var(--chakra-transition-property-common);
+      transition-duration: var(--chakra-transition-duration-fast);
+      transition-timing-function: var(--chakra-transition-easing-ease-out);
+      cursor: pointer;
+      text-decoration: none;
+      outline: transparent solid 2px;
+      outline-offset: 2px;
+      border: 0px solid transparent;
+      position: relative;
+      color: var(--chakra-colors-brand-logoColor);
     }
   }
 }
-
-.content {
-  flex: 1;
-  padding: 1rem;
-  margin-left: 250px;
-
-  &.is-mobile {
-    margin-left: 0;
-  }
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  z-index: 50;
-}
-
-@media (max-width: 768px) {
+@media screen and (min-width: 80em) {
   .sidebar {
-    position: absolute;
-    transform: translateX(-100%);
-
-    &.active {
-      transform: translateX(0);
-    }
-  }
-
-  .content {
-    margin-left: 0;
+    transform: none;
+    width: 315px;
   }
 }
 </style>
